26363c8bdfa6551fc4a13ba670301385dc7950e3
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-06-01 21:35:33 -0700

    react-router-dom v 5 to use switch route. moved routes to Home component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import Contact from "./Contact/Contact";
+
+import Home from "./Home/Home";
 
 function App() {
+
+  
   return (
     <Router>
       <div className="App">
-        <h1>test</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+        <Home/>
+        </div>
+        </Router>
   );
+  
 }
 
 export default App;
