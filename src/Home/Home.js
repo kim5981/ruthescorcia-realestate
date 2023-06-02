8a058a572d7e090fbfe3c4eef100ed5f32b78736
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-06-01 23:12:40 -0700

    adds media for home icon, logo section on home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+import "./Home.css";
+import house from "../media/icons8-house-48.png";
+
 export default function Home() {
   
   return (
       <>
+        <div id="logo-wrap">
+            <img src={house} alt="icon of a house" className="logo icon"/>
+            <h4 className="logo">RUTH ESCORCIA</h4>
+        </div>
         <h1>Welcome to Home Page</h1>
+        
         <Link to="/home">Home</Link>
         <Link to="/contact">Contact</Link>
       </>
