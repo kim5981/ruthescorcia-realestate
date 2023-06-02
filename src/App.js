c70cc12414032f9d45aa352a1a720952d42f72a9
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-06-01 20:59:46 -0700

    adds route and link to contact component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import './App.css';
-import "./Contact/Contact"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Contact from "./Contact/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <h1>test</h1>
-      <Router>
+    <Router>
+      <div className="App">
+        <h1>test</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
 
-      </Router>
-    </div>
+        <Routes>
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
